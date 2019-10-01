09bd2c373c2bede3edf26eed7d65bf620633bc89
Marco Sälzer <uk030633@student.uni-kassel.de>
2019-10-01 17:24:54 +0200

    buffer simulation (example) and edits
    created example simulating buffer behaviour
    (no description despite source code yet)
    
    changed node type to be able to create named nodes
    
    made lts add functions visible and removed some todos

--- src/datastructures.ml
+++ src/datastructures.ml
@@ -3,16 +3,34 @@ open Tcsbasedata
 open Tcsset
 
 (* NODE *)
-type node = int
-let node_to_string = string_of_int
+module Node = struct
+  type t =
+  | SimpleNode of int
+  | NamedNode of string
+
+  let to_string = function
+  SimpleNode i -> string_of_int i
+  | NamedNode s -> s
+
+  let compare = function
+  SimpleNode i -> (function
+              SimpleNode j -> Int.compare i j
+            | NamedNode t -> assert false)
+  | NamedNode s -> (function
+                      SimpleNode j -> assert false
+                    | NamedNode t -> String.compare s t)
+end 
+
+
+
 
 (* NODESET *)
 module NodeSet = struct
-  type t = node TreeSet.t
+  type t = Node.t TreeSet.t
 
   let isEmpty = TreeSet.is_empty
   let compare = TreeSet.compare
-  let empty = TreeSet.empty Int.compare
+  let empty = TreeSet.empty Node.compare
   let elem = TreeSet.mem
   let fold f acc ns = TreeSet.fold (fun x y -> f y x) ns acc
   let iter = TreeSet.iter
@@ -29,7 +47,7 @@ module NodeSet = struct
   let union = TreeSet.union
   let inter = TreeSet.inter
   let diff = TreeSet.diff
-  let of_node_list = TreeSet.of_list Int.compare
+  let of_node_list = TreeSet.of_list Node.compare
   let as_node_list = TreeSet.elements
   let fold_subsets = TreeSet.fold_subsets
 
@@ -46,12 +64,12 @@ module NodeSet = struct
 
   let max ns lessf = fold (fun v -> fun w -> if lessf v w then w else v) (some ns) ns
 
-  let to_string ns = "["^(String.concat "," (List.map node_to_string (as_node_list ns)))^"]"
+  let to_string ns = "["^(String.concat "," (List.map Node.to_string (as_node_list ns)))^"]"
 
   let init =
     let rec f ns = function
-      | 0 -> add 0 ns
-      | c -> f (add c ns) (c-1) in
+      | 0 -> add (Node.SimpleNode 0) ns
+      | c -> f (add (Node.SimpleNode c) ns) (c-1) in
       f empty
 end
 
--- src/datastructures.mli
+++ src/datastructures.mli
@@ -3,12 +3,16 @@ open Tcsset
 
 
 (* TODO: remove unused methods *)  
-(** Type for nodes. Currently  represented as integer value.
+(** Type for nodes.
     WARNING: May becomes abstract in the future.
 *)
-type node = int
+module Node : sig
+   type t = 
+      SimpleNode of int
+   |  NamedNode of string
 
-
+   val to_string : t -> string
+end
 (** Type for nodesets. Based on TreeSet.
     @see <https://github.com/tcsprojects/tcslib/blob/master/src/data/tcsset.ml> Treeset of TCSSet
 *)
@@ -23,7 +27,7 @@ module NodeSet : sig
 
    val compare : t -> t -> int
    
-   val elem    : node -> t -> bool
+   val elem    : Node.t -> t -> bool
    (** Checks if the given node is a member of the given nodeset.
       @param node node to be checked
       @param t set to check if node is included
@@ -35,7 +39,7 @@ module NodeSet : sig
       @return empty nodeset
    *)
 
-   val of_node_list    : node list -> t
+   val of_node_list    : Node.t list -> t
    (** Creates nodeset out of nodelist.
       @param nodelist list of nodes from which set should be created
       @return set of nodes from node list
@@ -47,16 +51,16 @@ module NodeSet : sig
       @return size of nodeset
    *)
                                                               
-   val fold    : ('a -> node -> 'a) -> 'a -> t -> 'a
+   val fold    : ('a -> Node.t -> 'a) -> 'a -> t -> 'a
    (** Fold nodeset. *)                                                        
 
-   val iter    : (node -> unit) -> t -> unit
+   val iter    : (Node.t -> unit) -> t -> unit
    (** Iterate nodeset. *)                                               
 
-   val map     : (node -> node) -> t -> t
+   val map     : (Node.t -> Node.t) -> t -> t
    (** Map nodeset. *)                                                
 
-   val filter  : (node -> bool) -> t -> t
+   val filter  : (Node.t -> bool) -> t -> t
    (** Filter nodeset via filter function
       
       @param (node -> bool) filter funciton
@@ -64,28 +68,28 @@ module NodeSet : sig
       @retrun filtered nodeset
    *)
 
-   val exists  : (node -> bool) -> t -> bool
+   val exists  : (Node.t -> bool) -> t -> bool
    (** Checks if via specified node does exist.
       @param (node -> bool) specifier
       @param t nodeset to be checked
       @return if node with specification exists
    *)                                                
 
-   val forall  : (node -> bool) -> t -> bool
+   val forall  : (Node.t -> bool) -> t -> bool
    (** Checks if specification holds for all nodes.
       @param (node -> bool) specifier
       @param t nodeset to be checked
       @return if specification holds for all nodes
    *)                                               
 
-   val find    : (node -> bool) -> t -> node
+   val find    : (Node.t -> bool) -> t -> Node.t
    (** Finds and returns specified node option.
       @param (node -> bool) specifier
       @param t nodeset to be searched
       @return if found Some node if not None
    *)                                                
 
-   val max     : t -> (node -> node -> bool) -> node
+   val max     : t -> (Node.t -> Node.t -> bool) -> Node.t
    (** Returns maximum node.
       The maximum is defined via given comparator function.
       @param t nodeset to be searched
@@ -93,33 +97,33 @@ module NodeSet : sig
       @return maximum node for comparator
    *)
 
-   val some    : t -> node
+   val some    : t -> Node.t
    (** Returns a randomly chosen element from a node set.
       
       @param t nodeset to get random node from
       @return random node
    *)
 
-   val first   : t -> node
+   val first   : t -> Node.t
    (** Returns the smallest (by name) node in a nodeset.
       @param nodeset nodeset to get smallest node from
       @return smallest node by name
    *)
 
-   val last    : t -> node 
+   val last    : t -> Node.t 
    (** Returns greatest (by name) node in a nodeset.
       @param t nodeset to get greatest node from
       @return greatest node by name
    *)
                         
-   val add     : node -> t -> t
+   val add     : Node.t -> t -> t
    (** Add node to nodeset.
       @param node node to add
       @param t nodeset to add to
       @return extended nodeset
    *)
 
-   val del     : node -> t -> t
+   val del     : Node.t -> t -> t
    (** Delete node from nodeset.
       @param node node to be deleted
       @param t nodeset to remove node from
@@ -149,7 +153,7 @@ module NodeSet : sig
 
    val fold_subsets : (t -> 'a -> 'a) -> t -> 'a -> 'a 
    
-   val as_node_list   : t -> node list
+   val as_node_list   : t -> Node.t list
    (** Extract a list of nodes from a nodeset.
       @param t nodeset to extract nodes as list from
       @return node list from nodeset 
